33a04bdf539d8d4902c481dfad2ab78b0449c7db
beursken <beursken@uni-duesseldorf.de>
2013-05-03 08:42:52 +0200

    Beginning of dynamic sprite loader

---
+++ Gruppe22/Gruppe22/Frontend/Map/Tileset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gruppe22
+{
+    public class Tileset
+    {
+        private string _fileName = "";
+        private List<TileObject> _walls;
+        private List<TileObject> _floor;
+    }
+}
