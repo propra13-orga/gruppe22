10730b1c55ae0a2985fe67d203bf89ce23263ac7
tron90 <ruslan.curbanov@uni-duesseldorf.de>
2013-05-18 15:17:42 +0200

    minimap MoveContend fixed
    Die MiniMap wird jetzt richtig verschoben

--- Gruppe22/Gruppe22/Frontend/Map/Minimap.cs
+++ Gruppe22/Gruppe22/Frontend/Map/Minimap.cs
@@ -109,6 +109,19 @@ namespace Gruppe22
             _spriteBatch.GraphicsDevice.RasterizerState.ScissorTestEnable = false;
             rstate.Dispose();
         }
+        
+        /// <summary>
+        /// Verschiebung der gedrehten Minimap. 
+        /// </summary>
+        /// <param name="difference"></param>
+        /// <param name="_lastCheck"></param>
+        public override void MoveContent(Vector2 difference, int _lastCheck = 0)
+        {
+            float tmp = 0.70710678f; //berechnete rückverschiebung (Rot.matrix für 45°), wurzel 2 halbe
+            difference = new Vector2(tmp*difference.X + (-tmp)*difference.Y, tmp*difference.X + tmp*difference.Y);
+            Move(difference);
+            base.MoveContent(difference);
+        }
 
         public void MoveCamera(Coords coords)
         {
