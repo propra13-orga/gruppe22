ab52dfee5fdaa7be6868d645e07f000e704aeb19
tron90 <ruslan.curbanov@uni-duesseldorf.de>
2013-06-30 22:20:38 +0200

    Abstract quest structure for the game story

---
+++ Gruppe22/Gruppe22/Backend/Map/Quest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe22.Backend
+{
+    public class Quest
+    {
+        private int _rewardXP;
+        private List<Item> _rewardItem;
+        private string _description;
+        private bool _done;
+
+        public bool IsDone { get { return _done; } }
+
+        public Quest(string text, int xp, List<Item> itemlist)
+        {
+            this._done = false;
+            this._description = text;
+            this._rewardItem = itemlist;
+            this._rewardXP = xp;
+        }
+
+        public virtual void Goal()
+        {
+            this._done = true;
+        }
+    }
+}
---
+++ Gruppe22/Gruppe22/Backend/Map/Story.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gruppe22.Backend
+{
+    public class Story
+    {
+        private NPC _questgiver;
+        private List<Quest> quests;
+
+        public Story(NPC questnpc, List<Quest> gamequests)
+        {
+            this._questgiver = questnpc;
+            this.quests = gamequests;
+        }
+    }
+}
